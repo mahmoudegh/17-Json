cb0d539713758f64aa29daab64e28dd959f24830
mahmoudegh <48510315+mahmoudegh@users.noreply.github.com>
2021-03-24 17:14:33 +0200

    Json

---
+++ #04 - Complex JSON Object And Compare With JS/main.js
@@ -0,0 +1,24 @@
+let productOne = {
+  name: "PC",
+  price: 3500,
+};
+
+let productTwo = {
+  name: "PC",
+  price: 3500,
+};
+
+let productThree = {
+  name: "PC",
+  price: 3500,
+};
+
+let productFour = {
+  name: "PC",
+  price: 3500,
+};
+
+console.log(productOne.name);
+console.log(productTwo.name);
+console.log(productThree.name);
+console.log(productFour.name);
